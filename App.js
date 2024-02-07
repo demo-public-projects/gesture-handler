deb661645e7de258fcc4e2c352fe6a439ac96a42
nrevutska <nrevutska@gmail.com>
2024-02-07 18:34:44 +0200

    withTimeout, withSpring

--- App.js
+++ App.js
@@ -3,31 +3,44 @@ import Animated, {
   useAnimatedStyle,
   withRepeat,
   withTiming,
+  withSpring,
 } from "react-native-reanimated";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import { useEffect } from "react";
 
 export default function App() {
   const progress = useSharedValue(100);
 
-  useEffect(() => {
-    progress.value = withRepeat(withTiming(progress.value + 100), 6, true);
-  }, []);
+  const startAnimation = () => {
+    progress.value = withTiming(
+      progress.value + 100,
+      { duration: 2000 },
+      () => {progress.value = withSpring(100, {
+        mass: 2.6,
+        damping: 8,
+        stiffness: 201,
+      });}
+    );
+  };
 
   const rstyle = useAnimatedStyle(() => ({
     width: progress.value,
     borderRadius: progress.value - 100,
+    transform: [{ translateX: progress.value * 2 - 300 }],
   }));
 
   return (
-    <View style={styles.container}>
+    <Pressable style={styles.container} onPress={startAnimation}>
       <Animated.View
-        style={[{
-          height: 100,
-          backgroundColor: "violet",
-        }, rstyle]}
+        style={[
+          {
+            height: 100,
+            backgroundColor: "violet",
+          },
+          rstyle,
+        ]}
       />
-    </View>
+    </Pressable>
   );
 }
 
