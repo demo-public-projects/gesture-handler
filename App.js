5a879bff707af9dc4d3cb9a87dd94d1420d0090c
nrevutska <nrevutska@gmail.com>
2024-02-07 11:50:40 +0200

    intro

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withRepeat,
+  withTiming,
+} from "react-native-reanimated";
+import { StyleSheet, Text, View } from "react-native";
+import { useEffect } from "react";
 
 export default function App() {
+  const progress = useSharedValue(100);
+
+  useEffect(() => {
+    progress.value = withRepeat(withTiming(progress.value + 100), 6, true);
+  }, []);
+
+  const rstyle = useAnimatedStyle(() => ({
+    width: progress.value,
+    borderRadius: progress.value - 100,
+  }));
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.View
+        style={[{
+          height: 100,
+          backgroundColor: "violet",
+        }, rstyle]}
+      />
     </View>
   );
 }
@@ -13,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
